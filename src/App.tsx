8de0a9a1fbf7a93219b6eb4dd065902f995bae3e
Alex K <aklimovskikh@yandex.ru>
2024-02-25 18:15:23 +0300

    Add color themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import "./styles/index.scss";
 import { Route, Routes, Link } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
-import "./index.scss";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
