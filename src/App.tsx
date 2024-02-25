a3b2bec7dab1a9139899df2fcb5a5ce13419c8ac
Alex K <aklimovskikh@yandex.ru>
2024-02-25 23:53:04 +0300

    Add classNames in helpers

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
