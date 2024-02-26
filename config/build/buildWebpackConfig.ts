09c9bcd66fecf754e7f9f38eb6d2534e8103f798
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-02-26 20:40:55 +0300

    Refactor to Feature-Sliced Design

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -15,7 +15,7 @@ export function buildWebpackConfig(
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     output: {
       filename: "[name].[contenthash].js",
       path: paths.build,
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import "./styles/index.scss";
 import { Route, Routes, Link } from "react-router-dom";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { MainPage } from "pages/MainPage";
+import { AboutPage } from "pages/AboutPage";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,8 +15,8 @@ const App = () => {
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/"} element={<MainPageAsync />} />
-          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPage />} />
+          <Route path={"/about"} element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
