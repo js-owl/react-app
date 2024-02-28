b4835d02a87fccc5c6569bb327678b7fa576ec11
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-02-28 14:14:20 +0300

    Add widgets/Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,5 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
@@ -9,7 +7,11 @@ const AppRouter = () => {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
-          <Route key={path} path={path} element={element} />
+          <Route
+            key={path}
+            path={path}
+            element={<div className="page-wrapper">{element}</div>}
+          />
         ))}
       </Routes>
     </Suspense>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -10,7 +9,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [])}>
-      <ThemeSwitcher />
       <div className={classNames(cls.links)}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
