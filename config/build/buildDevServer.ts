0a2c06c53a239fa20ef6d97e7690aae9e2fe4cfa
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-02-29 08:41:41 +0300

    Add Hot Module Replacement

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,5 +3,10 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 
 // historyApiFallback - убирает 404 ошибку при обновлении адресной строки
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
-  return { port: options.port, open: true, historyApiFallback: true };
+  return {
+    port: options.port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  };
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -19,5 +19,6 @@ export default function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ];
 }
