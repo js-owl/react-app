ce528aa11d373f493d3b48c0a46c5dca47f7e632
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-03-02 15:53:17 +0300

    Add NotFoundPage

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import './Loader.scss'
+
+interface LoaderProps {
+  className?: string
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+  return (
+    <div className={classNames('lds-circle', {}, [className])}>
+      <div></div>
+    </div>
+  )
+}
