a81f105ef008983ab0b8c9b2397bd2092db7e3c7
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-03-24 19:17:33 +0300

    Add if: always() in main.yaml

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -5,10 +5,8 @@ interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-  return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>/</div>
-    </div>
-  );
-};
+export const Navbar = ({ className }: NavbarProps) => (
+  <div className={classNames(cls.Navbar, {}, [className])}>
+    <div className={cls.links}>/</div>
+  </div>
+);
