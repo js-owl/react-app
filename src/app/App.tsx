bdb7da60aaf7591840a564272623f6c1e8a54aa3
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-03-28 23:13:39 +0300

    Move modal button to Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,19 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable i18next/no-literal-string */
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
   const { theme } = useTheme();
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
-        <button onClick={() => setIsOpen(true)}>toggle</button>
-        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ex ut
-          deleniti perspiciatis iure cumque quam quis suscipit similique fuga.
-        </Modal>
         <div className="content-page">
           <Sidebar />
           <AppRouter />
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,12 +1,35 @@
+/* eslint-disable i18next/no-literal-string */
+import { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-  <div className={classNames(cls.Navbar, {}, [className])}>
-    <div className={cls.links}>/</div>
-  </div>
-);
+export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev: boolean) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <Button
+        theme={ButtonTheme.CLEAR_INVERTED}
+        className={cls.links}
+        onClick={onToggleModal}
+      >
+        {t('Войти')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ex ut
+        deleniti perspiciatis iure cumque quam quis suscipit similique fuga.
+      </Modal>
+    </div>
+  );
+};
