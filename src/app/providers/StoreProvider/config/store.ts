39f1515c103a456a58246691a0749e92cd4f6a20
Alex K <aklimovskikh@yandex.ru>
2024-04-13 07:40:39 +0300

    Add User to StoreProvider

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { ReducersMapObject, configureStore } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { StateSchema } from './StateSchema';
+import { userReducer } from 'entities/User';
 
 export function createReduxStore(initialState?: StateSchema) {
+  const rootReducers: ReducersMapObject<StateSchema> = {
+    counter: counterReducer,
+    user: userReducer
+  };
+
   return configureStore<StateSchema>({
-    reducer: {
-      counter: counterReducer
-    },
+    reducer: rootReducers,
     devTools: __IS_DEV__,
     preloadedState: initialState
   });
