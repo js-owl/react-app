014d62b6adca8e20cd7247fd36f1a1d0d98bd45a
Alex K <aklimovskikh@yandex.ru>
2024-04-14 14:32:40 +0300

    Add storybook for Input and LoginForm

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { LoginForm } from './LoginForm';
+
+export default {
+  title: 'features/LoginForm',
+  component: LoginForm,
+  argTypes: {
+    backgroundColor: { control: 'color' }
+  }
+} as ComponentMeta<typeof LoginForm>;
+
+const Template: ComponentStory<typeof LoginForm> = (args) => (
+  <LoginForm {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
